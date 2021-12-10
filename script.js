7b1b7d26a2c1fc26c560e6ef1a0ce6d78f74893d
juspy <74178467+juspy@users.noreply.github.com>
2021-12-10 15:59:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+console.log('Чётные:');
+for (let i = 1; i < num.length; i++){
+    console.log(num[i]);
+    i = i + 1;
+}
+console.log('Нечётные:')
+for (let i = 0; i < num.length; i++){
+    console.log(num[i]);
+    i = i + 1;
+}
+
